2f98c247c79a05a0a7a412601f99fdb67edc14f3
Dominik Dufner <dominik.dufner@hpe.com>
2021-03-21 23:55:37 +0100

    ubuntu

--- ubuntuTest/foregroundCMDs.sh
+++ ubuntuTest/foregroundCMDs.sh
@@ -3,6 +3,7 @@
 echo "STARTE TUTORIAL ..." 
 docker run --name my-postgres -e POSTGRES_PASSWORD=password -d postgres
 sleep 1
+mkdir /json
 docker cp /root/json/data.json my-postgres:/json/data.json
 docker exec -it my-postgres bash
 sleep 2
