3739891c0803dab835c36983503b41427b7756ad
Dominik Dufner <dominik.dufner@hpe.com>
2021-03-21 21:34:53 +0100

    ubuntu

--- ubuntuTest/backgroundCMDs.sh
+++ ubuntuTest/backgroundCMDs.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 echo "WILLKOMMEN"
-docker run --name my-postgres -e POSTGRES_PASSWORD=password -d postgres
+#docker run --name my-postgres -e POSTGRES_PASSWORD=password -d postgres
 #echo "done" >> /opt/.backgroundfinished
-sleep 3
-echo "DOCKER EXEC"
-docker exec -it my-postgres bash
-sleep 1
-echo "PSQL"
-psql -U postgres
-echo "FERTIG"
--- ubuntuTest/foregroundCMDs.sh
+++ ubuntuTest/foregroundCMDs.sh
@@ -2,7 +2,9 @@
 
 echo "STARTE TUTORIAL ..." 
 #while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done;
-#docker exec -it my-postgres bash
-#sleep 1
-#psql -U postgres
+docker run --name my-postgres -e POSTGRES_PASSWORD=password -d postgres
+sleep 1
+docker exec -it my-postgres bash
+sleep 1
+psql -U postgres
 echo "FERTIG"
