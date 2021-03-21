54aaee1d9ea062ef9fb886014737fa291f28bb39
Dominik Dufner <dominik.dufner@hpe.com>
2021-03-21 21:41:35 +0100

    ubuntu

--- ubuntuTest/foregroundCMDs.sh
+++ ubuntuTest/foregroundCMDs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "STARTE TUTORIAL ..." 
-#while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done;
 docker run --name my-postgres -e POSTGRES_PASSWORD=password -d postgres
 sleep 1
 docker exec -it my-postgres bash
 sleep 1
 psql -U postgres
-echo "FERTIG"
+\! clear
