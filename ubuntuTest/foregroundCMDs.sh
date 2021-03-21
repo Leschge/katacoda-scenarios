0149bac7a641663e01d203d46fc5ac57b15b2a89
Dominik Dufner <dominik.dufner@hpe.com>
2021-03-21 21:27:17 +0100

    ubuntu

--- ubuntuTest/foregroundCMDs.sh
+++ ubuntuTest/foregroundCMDs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "BIN BEREIT"
+echo "Waiting to complete"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done;
+docker exec -it my-postgres bash
+psql -U postgres
+echo "done"
