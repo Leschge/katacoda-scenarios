fb30f05e4487d587b1908276863358542dd835fa
Dominik Dufner <dominik.dufner@hpe.com>
2021-03-21 21:28:11 +0100

    ubuntu

--- ubuntuTest/backgroundCMDs.sh
+++ ubuntuTest/backgroundCMDs.sh
@@ -2,7 +2,4 @@
 
 echo "WILLKOMMEN"
 docker run --name my-postgres -e POSTGRES_PASSWORD=password -d postgres
-sleep 5
-docker exec -it my-postgres bash
-sleep 5
-psql -U postgres
+echo "done" >> /opt/.backgroundfinished
