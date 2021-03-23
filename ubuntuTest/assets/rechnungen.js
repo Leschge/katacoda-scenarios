b854fb7e6a8a18a3ab32e0bc499324e1ec6e15b8
Leschge <44225383+Leschge@users.noreply.github.com>
2021-03-23 15:53:59 +0100

    Create rechnungen.js

---
+++ ubuntuTest/assets/rechnungen.js
@@ -0,0 +1,44 @@
+// rechnungsNr: 23774
+{ 
+	"Name":"Hans Peter", 
+	"Datum":"22-03-2021 08:23:00", 
+	"KundenNr": "2465433",
+	"Artikel":[
+		{ "Name": "Pullover", "Preis": "35.00", "Menge": "1" },
+		{ "Name": "Socken", "Preis": "7.95", "Menge": "3" },
+		{ "Name": "Jeans", "Preis": "65.99", "Menge": "1" }
+	]
+}
+
+// rechnungsNr: 23775
+{ 
+	"Name":"Sabine Meerson", 
+	"Datum":"22-03-2021 10:45:00", 
+	"KundenNr": "2465434",
+	"Artikel":[
+		{ "Name": "Bluse", "Preis": "35.00", "Menge": "1" },
+		{ "Name": "Schal", "Preis": "6.95", "Menge": "2" }
+	]
+}
+
+// rechnungsNr: 23776
+{ 
+	"Name":"Michael Müller", 
+	"Datum":"23-03-2021 13:32:00", 
+	"KundenNr": "2465435",
+	"Artikel":[
+		{ "Name": "Hose", "Preis": "45.00", "Menge": "2" },
+		{ "Name": "Shirt", "Preis": "24.99", "Menge": "3" },
+		{ "Name": "Sneaker", "Preis": "95.99", "Menge": "1" }
+	]
+}
+
+// rechnungsNr: 23777
+{ 
+	"Name":"Jonas Schell", 
+	"Datum":"23-03-2021 19:03:00", 
+	"KundenNr": "2465436",
+	"Artikel":[
+		{ "Name": "Shorts", "Preis": "35.00", "Menge": "2" }
+	]
+}
