2a90d3611bc650f389462e1d91938317a758ed2c
Leschge <44225383+Leschge@users.noreply.github.com>
2021-03-24 16:52:47 +0100

    Update StrukturRechnung.js

--- ubuntuTest/assets/StrukturRechnung.js
+++ ubuntuTest/assets/StrukturRechnung.js
@@ -1,9 +1,9 @@
-Schlüssel = rechnung::23744
+// rechnungsNr: 23774
 { 
-	"Name":"Hans Peter", 
-	"Datum":"22-03-2021 23:32:00", 
+	"Name": "Hans Peter", 
+	"Datum": "22-03-2021 23:32:00", 
 	"KundenNr": "2465433",
-	"Adresse": { "Strasse": "Silberalle 12", "PLZ": "73357","Ort": "München" },
+	"Adresse": { "Strasse": "Silberalle 12", "PLZ": "73357", "Ort": "München" },
 	"Artikel":[
 		{ "Name": "Pullover", "Preis": "35.00" },
 		{ "Name": "Socken", "Preis": "7.95" },
