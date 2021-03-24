90cff1ba058f15feb379ecb0718f4db1ec7c1e9c
Leschge <44225383+Leschge@users.noreply.github.com>
2021-03-24 16:27:33 +0100

    Create StrukturRechnung.js

---
+++ ubuntuTest/assets/StrukturRechnung.js
@@ -0,0 +1,12 @@
+Schlüssel = rechnung::23744
+{ 
+	"Name":"Hans Peter", 
+	"Datum":"22-03-2021 23:32:00", 
+	"KundenNr": "2465433",
+	"Adresse": { "Strasse": "Silberalle 12", "PLZ": "73357","Ort": "München" },
+	"Artikel":[
+		{ "Name": "Pullover", "Preis": "35.00" },
+		{ "Name": "Socken", "Preis": "7.95" },
+		{ "Name": "Jeans", "Preis": "65.99" }
+	]
+}
