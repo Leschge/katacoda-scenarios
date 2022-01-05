4f9bc78d926139d57011cc4754ec66e173880592
ddomnik <44225383+ddomnik@users.noreply.github.com>
2022-01-05 20:23:21 +0100

    Update foregroundCMDs.sh

--- postgresql-and-json/foregroundCMDs.sh
+++ postgresql-and-json/foregroundCMDs.sh
@@ -2,7 +2,7 @@
 
 echo "STARTE TUTORIAL ..." 
 docker run --name my-postgres -e POSTGRES_PASSWORD=password -d postgres
-sleep 5
+sleep 10
 mkdir /root/json
 docker cp /root/json/data.json my-postgres:/json/data.json
 docker cp /root/json/data.json my-postgres:/data.json
